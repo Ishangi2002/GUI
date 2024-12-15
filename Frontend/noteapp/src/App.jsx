2aac2f5cd673831e63490709297cd1e405205402
Ishangi2002 <ishashinthya@gmail.com>
2024-12-16 00:34:57 +0530

    first commit

---
+++ Frontend/noteapp/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="bg-blue-500 text-white ">
+            <h1 className="text-2xl ">Tailwind CSS is Working!</h1>
+            <p>This background should be blue, and the text white.</p>
+            <p>hii</p>
+    </div>
+  );
+}
+
+export default App
